802e3c9c2fc8af6739501a25c15fbe05db12939e
Nathanael Farley <nasfarley88@gmail.com>
2016-11-30 22:28:01 +0000

    Fully switched over to telepot.

--- __init__.py
+++ __init__.py
@@ -1,102 +1,98 @@
 import logging
 import re
-from enum import Enum
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackQueryHandler, ConversationHandler
-from telegram.ext.dispatcher import run_async
-from skybeard.beards import Beard
-
+import telepot
+import telepot.aio
+from telepot import glance
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from skybeard.beards import BeardAsyncChatHandlerMixin, ThatsNotMineException
 
 logger = logging.getLogger(__name__)
 
-def drop_prefix(data):
-    return data[3:]
 
-class NamedVoteBeard(Beard):
+class NamedVoteBeard(telepot.aio.helper.ChatHandler,
+                     BeardAsyncChatHandlerMixin):
     """Named voting for skybeard-2
 
     Type /nvtest to make test poll.
     Type /nvask to ask a specific yes/no question.
     """
 
-    class YesNoQuestionEnum(Enum):
-        ASKING = 1
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.register_command("nvtest", self.test)
+        self.register_command("nvask", self.ask_question)
 
-    def initialise(self):
-        self.disp.add_handler(CommandHandler("nvtest", self.test))
+        self.yes_no_maybe = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(
+                    text="a) Yes",
+                    callback_data=self.serialize('a)')),
+                 InlineKeyboardButton(
+                     text="b) No",
+                     callback_data=self.serialize('b)'))],
+                [InlineKeyboardButton(
+                    text="c) Maybe",
+                    callback_data=self.serialize('c)'))],
+            ])
 
-        # Conversation for a simple yes no question
-        self.disp.add_handler(ConversationHandler(
-            entry_points=[CommandHandler('nvask', self.ask_what)],
-            states={
-                self.YesNoQuestionEnum.ASKING: [MessageHandler(Filters.text, self.ask), ]
-            },
-            fallbacks=[CommandHandler('cancel', lambda x, y: ConversationHandler.END)]
-        ))
+    async def _post_quiz(self, text, reply_markup):
+        await self.sender.sendMessage(
+            '{}\na)\nb)\nc)'.format(text),
+            reply_markup=reply_markup)
 
-        self.disp.add_handler(CallbackQueryHandler(self.update_quiz))
+    async def ask_question(self, msg):
+        await self.sender.sendMessage("Sup. What's the question?")
+        query_msg = await self.listener.wait()
+        await self._post_quiz(
+            text=query_msg['text'],
+            reply_markup=self.yes_no_maybe)
 
-        self.yes_no_maybe = InlineKeyboardMarkup([
-            [InlineKeyboardButton("a) Yes", callback_data='nvta)'),
-             InlineKeyboardButton("b) No", callback_data='nvtb)')],
-            [InlineKeyboardButton("c) Maybe", callback_data='nvtc)')],
-        ])
+    async def test(self, msg):
+        await self._post_quiz(
+            text="Select an answer:",
+            reply_markup=self.yes_no_maybe)
 
+    async def on_callback_query(self, msg):
+        query_id, from_id, query_data = glance(msg, flavor='callback_query')
+        try:
+            data = self.deserialize(query_data)
+        except ThatsNotMineException:
+            return
 
-    # YesNoQuestion code
-    def ask_what(self, bot, update):
-        update.message.reply_text("Sup. What's your question?")
+        if msg['from']['last_name']:
+            name = "{} {}".format(
+                msg['from']['first_name'], msg['from']['last_name'])
+        else:
+            name = msg['from']['first_name']
 
-        return self.YesNoQuestionEnum.ASKING
+        text_as_list = msg['message']['text'].split("\n")
+        for i in range(len(text_as_list)):
+            if text_as_list[i][:2] == data:
+                if name in text_as_list[i]:
+                    text_as_list[i] = await self.remove_name(
+                        text_as_list[i], name)
+                else:
+                    text_as_list[i] = await self.add_name(
+                        text_as_list[i], name)
 
-    def ask(self, bot, update):
-        update.message.reply_text('{}\na)\nb)\nc)'.format(update.message.text), reply_markup=self.yes_no_maybe)
+        new_text = "\n".join(text_as_list)
 
-        return ConversationHandler.END
+        # TODO set up to take the keyboard from the query
+        await self.bot.editMessageText(
+            telepot.origin_identifier(msg),
+            text=new_text,
+            reply_markup=self.yes_no_maybe)
 
-    def test(self, bot, update):
-        update.message.reply_text('Please choose:\na)\nb)\nc)', reply_markup=self.yes_no_maybe)
-
-
-    def add_name(self, text, name):
+    async def add_name(self, text, name):
         if text[-1] == ")":
             return "{} {}".format(text, name)
         else:
             return "{}, {}".format(text, name)
 
-    def remove_name(self, text, name):
+    async def remove_name(self, text, name):
         retval = re.sub(r"\s{},?".format(name), "", text)
         if retval[-1] == ",":
             retval = retval[:-1]
         retval = retval.strip()
         return retval
-
-
-    def update_quiz(self, bot, update):
-        query = update.callback_query
-
-        text_as_list = query.message.text.split("\n")
-
-        data = drop_prefix(query.data)
-
-        if query.from_user.last_name:
-            name = "{} {}".format(
-                query.from_user.first_name, query.from_user.last_name)
-        else:
-            name = query.from_user.first_name
-
-        for i in range(len(text_as_list)):
-            if text_as_list[i][:2] == data:
-                if name in text_as_list[i]:
-                    text_as_list[i] = self.remove_name(text_as_list[i], name)
-                else:
-                    text_as_list[i] = self.add_name(text_as_list[i], name)
-
-        new_text = "\n".join(text_as_list)
-
-        # TODO set up to take the keyboard from the query
-        bot.editMessageText(text=new_text,
-                            chat_id=query.message.chat_id,
-                            message_id=query.message.message_id,
-                            reply_markup=self.yes_no_maybe)
