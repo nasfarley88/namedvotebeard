7bcddf3c3f9d16f5d15164ba089a01ade52bb3a1
Nathanael Farley <nasfarley88@gmail.com>
2017-02-20 21:07:37 +0000

    Now works with new BeardDBTable.

--- python/namedvotebeard/__init__.py
+++ python/namedvotebeard/__init__.py
@@ -1,10 +1,12 @@
 import logging
 import re
 import string
+from functools import partial
 
-import dataset
-#import yaml
-import pickle
+try:
+    import dill as pickle
+except ImportError:
+    import pickle
 
 import telepot
 import telepot.aio
@@ -12,32 +14,34 @@ from telepot import glance
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from skybeard.beards import BeardChatHandler, ThatsNotMineException
 from skybeard import utils, decorators
+from skybeard.utils import get_args
+from skybeard.bearddbtable import BeardDBTable
 
 logger = logging.getLogger(__name__)
 
 
-def get_args(msg):
-    return msg['text'].split(" ")[1:]
+# def get_args(msg):
+#     return msg['text'].split(" ")[1:]
 
-DB_NAME = "sqlite:///namedvotebeard.db"
+# DB_NAME = "sqlite:///namedvotebeard.db"
 
-class BeardDBTable(object):
-    """Placeholder class for getting a table."""
+# class BeardDBTable(object):
+#     """Placeholder class for getting a table."""
 
-    def __init__(self, beard, table_name):
-        self.beard = beard
-        self.table_name = beard.get_name()+"_"+table_name
+#     def __init__(self, beard, table_name):
+#         self.beard = beard
+#         self.table_name = beard.get_name()+"_"+table_name
 
-    def __enter__(self):
-        self.db = dataset.connect(DB_NAME)
-        self.table = self.db[self.table_name]
-        return self.table
+#     def __enter__(self):
+#         self.db = dataset.connect(DB_NAME)
+#         self.table = self.db[self.table_name]
+#         return self.table
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.db.commit()
-        del self.db
-        del self.table
-        return
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         self.db.commit()
+#         del self.db
+#         del self.table
+#         return
 
 class NamedVoteBeard(BeardChatHandler):
 
@@ -54,18 +58,25 @@ class NamedVoteBeard(BeardChatHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.yes_no_maybe = InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton(
-                    text="a) Yes",
-                    callback_data=self.serialize('a)')),
-                 InlineKeyboardButton(
-                     text="b) No",
-                     callback_data=self.serialize('b)'))],
-                [InlineKeyboardButton(
-                    text="c) Maybe",
-                    callback_data=self.serialize('c)'))],
-            ])
+        self.test = partial(self.vote_any,
+                            question="Test question?",
+                            responses=["Yes", "No", "Foo bar?"])
+        self.ask_question = partial(self.vote_any,
+                                    responses=["Yes", "No", "Maybe"])
+        self.messages_table = BeardDBTable(self, "messages")
+
+        # self.yes_no_maybe = InlineKeyboardMarkup(
+        #     inline_keyboard=[
+        #         [InlineKeyboardButton(
+        #             text="a) Yes",
+        #             callback_data=self.serialize('a)')),
+        #          InlineKeyboardButton(
+        #              text="b) No",
+        #              callback_data=self.serialize('b)'))],
+        #         [InlineKeyboardButton(
+        #             text="c) Maybe",
+        #             callback_data=self.serialize('c)'))],
+        #     ])
 
     async def make_keyboard(self, items):
         keyboard = InlineKeyboardMarkup(
@@ -77,38 +88,48 @@ class NamedVoteBeard(BeardChatHandler):
         return keyboard
 
     @decorators.onerror("Sorry, something went wrong. Perhaps too few arguments?")
-    async def vote_any(self, msg):
-        args = utils.get_args(msg['text'])
-        items = [x.strip() for x in args[1].split(",")]
-        keyboard = await self.make_keyboard(items)
-        text = "{}\n{}".format(args[0],
-                               "\n".join([string.ascii_letters[x]+")" for x in range(len(items))]))
+    async def vote_any(self, msg=None, question=None, responses=None):
+        if msg is not None:
+            args = utils.get_args(msg['text'])
+        if question is None:
+            question = args[0]
+        if responses is None:
+            responses = [x.strip() for x in args[1].split(",")]
+        keyboard = await self.make_keyboard(responses)
+        text = "{}\n{}".format(question,
+                               "\n".join([string.ascii_letters[x]+")" for x in range(len(responses))]))
         msg = await self.sender.sendMessage(text, reply_markup=keyboard)
-        with BeardDBTable(NamedVoteBeard, "messages") as table:
-            table.insert(dict(msg_id=msg['message_id'], msg=pickle.dumps(msg), keyboard=pickle.dumps(items)))
+        with self.messages_table as table:
+            table.insert(
+                dict(
+                    msg_id=msg['message_id'],
+                    msg=pickle.dumps(msg),
+                    keyboard=pickle.dumps(responses)
+                ))
 
-    async def _post_quiz(self, text, reply_markup):
-        await self.sender.sendMessage(
-            '{}\na)\nb)\nc)'.format(text),
-            reply_markup=reply_markup)
 
-    async def ask_question(self, msg):
-        args = get_args(msg)
-        if args:
-            await self._post_quiz(
-                text=" ".join(args),
-                reply_markup=self.yes_no_maybe)
-        else:
-            await self.sender.sendMessage("Sup. What's the question?")
-            query_msg = await self.listener.wait()
-            await self._post_quiz(
-                text=query_msg['text'],
-                reply_markup=self.yes_no_maybe)
+    # async def _post_quiz(self, text, reply_markup):
+    #     await self.sender.sendMessage(
+    #         '{}\na)\nb)\nc)'.format(text),
+    #         reply_markup=reply_markup)
 
-    async def test(self, msg):
-        await self._post_quiz(
-            text="Select an answer:",
-            reply_markup=self.yes_no_maybe)
+    # async def ask_question(self, msg):
+    #     args = get_args(msg)
+    #     if args:
+    #         await self._post_quiz(
+    #             text=" ".join(args),
+    #             reply_markup=self.yes_no_maybe)
+    #     else:
+    #         await self.sender.sendMessage("Sup. What's the question?")
+    #         query_msg = await self.listener.wait()
+    #         await self._post_quiz(
+    #             text=query_msg['text'],
+    #             reply_markup=self.yes_no_maybe)
+
+    # async def test(self, msg):
+    #     await self._post_quiz(
+    #         text="Select an answer:",
+    #         reply_markup=self.yes_no_maybe)
 
     async def on_callback_query(self, msg):
         query_id, from_id, query_data = glance(msg, flavor='callback_query')
@@ -141,7 +162,7 @@ class NamedVoteBeard(BeardChatHandler):
         #    text=new_text,
         #    reply_markup=self.yes_no_maybe)
         self.logger.debug(str(msg))
-        with BeardDBTable(NamedVoteBeard, "messages") as table:
+        with self.messages_table as table:
             entry = table.find_one(msg_id=msg['message']['message_id'])
 
         await self.bot.editMessageText(
